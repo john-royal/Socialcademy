028069628a8747539488b4ec6cc2d621a77cb803
John Royal <34844819+john-royal@users.noreply.github.com>
2021-10-05 15:15:18 -0400

    chore: init

---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by John Royal on 10/5/21.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
