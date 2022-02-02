1eeaa0eaa4b4badc1d7b991a85291f13570e8d4b
John Royal <34844819+john-royal@users.noreply.github.com>
2022-02-01 19:25:52 -0500

    fix: add name to local user after account creation

--- Socialcademy/Services/AuthService.swift
+++ Socialcademy/Services/AuthService.swift
@@ -26,9 +26,8 @@ class AuthService: ObservableObject {
     
     func createAccount(name: String, email: String, password: String) async throws {
         let result = try await auth.createUser(withEmail: email, password: password)
-        let profileChangeRequest = result.user.createProfileChangeRequest()
-        profileChangeRequest.displayName = name
-        try await profileChangeRequest.commitChanges()
+        try await result.user.updateProfile(\.displayName, to: name)
+        user?.name = name
     }
     
     func signOut() throws {
@@ -58,5 +57,13 @@ private extension User {
         self.id = firebaseUser.uid
         self.name = firebaseUser.displayName ?? ""
         self.imageURL = firebaseUser.photoURL
+    }
+}
+
+private extension FirebaseAuth.User {
+    func updateProfile<T>(_ keyPath: WritableKeyPath<UserProfileChangeRequest, T>, to newValue: T) async throws {
+        var profileChangeRequest = createProfileChangeRequest()
+        profileChangeRequest[keyPath: keyPath] = newValue
+        try await profileChangeRequest.commitChanges()
     }
 }
